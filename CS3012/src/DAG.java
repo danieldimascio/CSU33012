c382c251726fb2f2757cf9fc8c5b9fcf0047bb38
danieldimascio <55289962+danieldimascio@users.noreply.github.com>
2019-10-17 19:28:49 +0100

    Finished DAG for now

--- CS3012/src/DAG.java
+++ CS3012/src/DAG.java
@@ -7,8 +7,8 @@ public class DAG
 	private boolean[] stack;
 	private int vert;						//number of vertices
 	private int edge;						//number of edges
-	private ArrayList<Integer>[]adjacent;	//adjacent = adjacency list for vertex vert
-	private int[] indegree;					//indegree = in-degree of vertex vert
+	private ArrayList<Integer>[]adjacent;	// = adjacency list for vert
+	private int[] inDeg;					// = in-degree of vert
 	private boolean[] visited;				//list of visited vertices
 	private boolean cycles;					//Does the graph have a cycle?			
 	
@@ -33,7 +33,7 @@ public class DAG
 		
 		this.vert = vert;
 		this.edge = 0;
-		indegree = new int[vert];
+		inDeg = new int[vert];
 		visited = new boolean[vert];
 		stack = new boolean[vert];
 		adjacent = (ArrayList<Integer>[]) new ArrayList[vert];
@@ -62,36 +62,141 @@ public class DAG
 	}
 	
 	//Directed edges to v
-		public int indegree(int v)
+	public int indegree(int v)
+	{
+		if(validate(v))
 		{
-			if(validate(v))
-			{
-				return indegree[v];
-			}
-			else
-				return -1;
-			
+			return inDeg[v];
 		}
+		else
+			return -1;
 		
-		//Directed edges from v
-		public int outdegree(int v)
-		{
-			if(validate(v))
-			{
-				return adjacent[v].size();
-			}
-				return -1;
-		}
+	}
 	
+	//Directed edges from v
+	public int outdegree(int v)
+	{
+		if(validate(v))
+		{
+			return adjacent[v].size();
+		}
+			return -1;
+	}
+
 	//Add directed edge from u to v
 	public void addEdge(int u, int v)
 	{
 		if(validate(u) && validate(v))
 		{
 			adjacent[u].add(v);
-			indegree[v]++;
+			inDeg[v]++;
 			edge++;
 		}
 		else{ System.out.println("Please only include figures from 1 to " + (vert-1)); }		
+	}
+	
+	public void cycles(int v)
+	{
+		stack[v] = true;
+		visited[v] = true;
+		
+		for(int u:adjacent(v))
+		{
+			if(!visited[u]){
+				cycles(u);
+			}
+			
+			else if(stack[u]){
+				cycles = true;
+				return;
+			}
+		}
+		stack[v] = false;
+	}
+	
+	public ArrayList<Integer> breadthFirst(int source) 				//print out a Breadth-First Search
+	{
+		ArrayList<Integer> arr = new ArrayList<Integer>();
+		LinkedList<Integer> inQueue = new LinkedList<Integer>();
+		boolean mark[] = new boolean[vert]; 						//Marks vertices as not visited
+		
+		mark[source] = true;
+		inQueue.add(source);
+		
+		while(inQueue.size()!=0)
+		{
+			source = inQueue.poll(); 								//polls source
+			arr.add(source);
+			Iterator<Integer> i = adjacent[source].listIterator();	//Find, mark and queue adjacent vertices.
+			
+			while(i.hasNext())
+			{
+				int n = i.next();
+				if(!mark[n])
+				{
+					mark[n] = true;
+					inQueue.add(n);
+				}
+			}
+		}
+		return arr;
+	}
+	
+	//Reverse DAG
+	public DAG reverseDAG()
+	{
+		DAG reverseDAG = new DAG(vert);
+		for(int v = 0; v <vert; v++)
+		{
+			for(int u:adjacent(v))
+			{
+				reverseDAG.addEdge(u, v);
+			}		
+		}
+		return reverseDAG;
+	}
+	
+	//LCA function
+	public int findLCA(int u, int v)
+	{
+		cycles(0);
+		
+		if(cycles)
+		{
+			return -1;
+		}
+		else if(!validate(u) || !validate(v))
+		{
+			return -1;
+		}
+		else if(edge == 0) //empty graph 
+		{
+			return -1;
+		}
+		
+		DAG reverseDAG = reverseDAG();
+		
+		ArrayList<Integer> inCommon = new ArrayList<Integer>();
+		ArrayList<Integer> a1 = reverseDAG.breadthFirst(u);
+		ArrayList<Integer> a2 = reverseDAG.breadthFirst(v);
+		
+		boolean isFound = false;
+		
+		for(int i = 0; i<a1.size(); i++)
+		{
+			for(int j = 0; j<a2.size(); j++)
+			{
+				if(a1.get(i) == a2.get(j))
+				{
+					inCommon.add(a1.get(i));
+					isFound = true;
+				}
+			}
+		}
+		if(isFound)	//Return LCA
+		{
+			return inCommon.get(0);
+		}
+		return -1;
 	}
 }	
