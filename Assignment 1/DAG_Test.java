6c32184f60ef7e11bb35b7d293c5c108eb4c45d3
danieldimascio <55289962+danieldimascio@users.noreply.github.com>
2019-10-18 15:43:36 +0100

    ompleted DAG_Test

--- Assignment 1/DAG_Test.java
+++ Assignment 1/DAG_Test.java
@@ -78,7 +78,16 @@ public class DAG_Test {
 		test.addEdge(2, 0);
 		
 		test.doesCycle(0);
-		assertTrue(test.cycles());	//DAG loops back, cycle = true
+		assertTrue(test.cycles());		//DAG loops back, cycle = true
+		
+		DAG test2 = new DAG(5);
+		
+		test2.addEdge(0, 1);
+		test2.addEdge(1, 2);
+		test2.addEdge(2, 3);
+		
+		test2.doesCycle(0);
+		assertFalse(test2.cycles());	//DAG does not loop, cycle = false
 	}
 	
 	@Test
@@ -114,5 +123,27 @@ public class DAG_Test {
 		assertFalse(test.validate(55));		//doesn't work as 55 > 5
 		assertFalse(test.validate(-55));	//doesn't work as -55 < 0
 		assertTrue(test.validate(3));		//works; 3 < 5 and 3 > 0
+	}
+
+	@Test
+	public void testLCA()
+	{
+		DAG test = new DAG(16);
+		
+		test.addEdge(1, 2);
+		test.addEdge(2, 4);
+		test.addEdge(3, 8);
+		test.addEdge(4, 12);
+		test.addEdge(5, 16);
+		
+		assertEquals(test.LCA(2, 4), 2);	//works
+		assertEquals(test.LCA(4, 5), -1);	//none found
+		assertEquals(test.LCA(4, 4), 4);	//test if a vertex returns itself
+		assertEquals(test.LCA(3, 16), -1);	//test if one vertex is not in the tree, error message
+		assertEquals(test.LCA(9, 7), -1);	//test if both vertex are not in the tree
+		assertEquals(test.LCA(-2, 3), -1);	//test negative vertex
+		
+		DAG test2 = new DAG(5);
+		assertEquals(test2.LCA(1, 2), -1);	//test for an empty DAG
 	}
 }
